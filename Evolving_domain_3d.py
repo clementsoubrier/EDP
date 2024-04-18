da8cbe1a21249723bc267700817bf2f00fa3c3a1
Clément Soubrier <c.soubrier@math.ubc.ca>
2024-04-18 14:58:39 -0700

    3D version

--- Evolving_domain_3d.py
+++ Evolving_domain_3d.py
@@ -12,31 +12,94 @@ import scipy
 import matplotlib.pyplot as plt
 import matplotlib.colors as mplc
 
+import gmsh
+
 
 import ufl
 from basix.ufl import element, mixed_element
 from dolfinx import default_real_type, log, plot, mesh
 from dolfinx.fem import Function, functionspace, Expression, FunctionSpaceBase, assemble_matrix, form
 from dolfinx.fem.petsc import NonlinearProblem
-from dolfinx.io import XDMFFile
+from dolfinx.io import XDMFFile, gmshio
 from dolfinx.mesh import CellType, create_unit_square, GhostMode
 from dolfinx.nls.petsc import NewtonSolver
 from ufl import dx, grad, inner, SpatialCoordinate
 
 
-def update_msh(m, v0, v1, midpoint):
-    x_cord = m.geometry.x[:,0]
-    mov = np.zeros(np.shape(x_cord))
 
-    x_2 = x_cord[-1]
-    x_1 = x_cord[0]
+
+def create_3d_cell_mesh(name):
+    """Create a mesh of a rod shape bacterium 
+
+    Args:
+        name: Name (identifier) of the mesh to add.
+
+    Returns:
+        dolfinx mesh
+
+    """
+    gmsh.initialize()
+    gmsh.option.setNumber("General.Terminal", 0)
+
+    # Create model
+    model = gmsh.model()
     
 
-    mov[:midpoint] = v0*(x_cord[:midpoint] - x_cord[midpoint])/(x_cord[midpoint]-x_1)
-    mov[midpoint+1:] = v1*(x_cord[midpoint+1:] - x_cord[midpoint])/(x_2-x_cord[midpoint])
+    model.add(name)
+    model.setCurrent(name)
+
+    cyl_dim_tags = model.occ.addCylinder(-1.5, 0, 0, 3, 0, 0, 0.5)
+    sphere1_dim_tags = model.occ.addSphere(-1.5, 0, 0, 0.5)
+    sphere2_dim_tags = model.occ.addSphere(1.5, 0, 0, 0.5)
+    model_dim_tags = model.occ.fuse([(3, cyl_dim_tags)], [(3, sphere1_dim_tags),(3, sphere2_dim_tags)])
+    model.occ.synchronize()
+
+    # Add physical tag 1 for exterior surfaces
+    boundary = model.getBoundary(model_dim_tags[0], oriented=False)
+    boundary_ids = [b[1] for b in boundary]
+    model.addPhysicalGroup(2, boundary_ids, tag=1)
+    model.setPhysicalName(2, 1, "Surface")
+
+    # Add physical tag 2 for the volume
+    volume_entities = [model[1] for model in model.getEntities(3)]
+    model.addPhysicalGroup(3, volume_entities, tag=2)
+    model.setPhysicalName(3, 2, "Volume")
+    gmsh.option.setNumber("Mesh.MeshSizeFactor", 0.5)
+    # gmsh.option.setNumber("Mesh.MeshSizeMax", 0.3)
+    # gmsh.option.setNumber("Mesh.MeshSizeMax", 0.3)
+    model.mesh.generate(dim=3)
+    
+    model.setCurrent("cell")
+    
+    filename = f"out_gmsh/mesh_rank_{MPI.COMM_WORLD.rank}.xdmf"
+    msh, ct, ft = gmshio.model_to_mesh(model, MPI.COMM_SELF, rank=0)
+    msh.name = name
+    ct.name = f"{msh.name}_cells"
+    ft.name = f"{msh.name}_facets"
+    with XDMFFile(msh.comm, filename, "w") as file:
+        msh.topology.create_connectivity(2, 3)
+        file.write_mesh(msh)
+        file.write_meshtags(
+            ct, msh.geometry, geometry_xpath=f"/Xdmf/Domain/Grid[@Name='{msh.name}']/Geometry"
+        )
+        file.write_meshtags(
+            ft, msh.geometry, geometry_xpath=f"/Xdmf/Domain/Grid[@Name='{msh.name}']/Geometry"
+        )
+    return msh
 
 
-    m.geometry.x[:,0] = m.geometry.x[:,0]+mov
+
+
+def update_msh(m, v0, v1):
+    x_cord = m.geometry.x[:,0]
+    min_x = np.min(x_cord)
+    max_x = np.max(x_cord)
+    d0 = (min_x-v0)/min_x
+    d1 = (max_x+v1)/max_x
+    mov = d0 * (x_cord<0).astype(float) + d1 * (x_cord>0).astype(float)
+
+
+    m.geometry.x[:,0] = m.geometry.x[:,0]*mov
     
     
     
@@ -57,17 +120,14 @@ def run_simulation():
     # Next, various model parameters are defined:
 
     dt = 5.0e-04            # time step
-    step_number = 3000      # time step number
+    step_number = 5000      # time step number
     time_range = np.linspace(0, step_number* dt,  step_number+1)
-    norm_stop = 0.1e-6      
     
-    cell_number = 100       # cell number for the mesh
-    v_0 = 0.       # speed of left pole
-    v_1 = 0.0004           # speed of right pole
-    mid_point = cell_number//2
+    v_0 = 0.0005        # speed of left pole
+    v_1 = 0.0005           # speed of right pole
 
-    uv_array = np.zeros((step_number+1, cell_number+1, 2))  # initalize solutions
-    x_array = np.zeros((step_number+1, cell_number+1))      # initalize mesh geometry
+    # uv_array = np.zeros((step_number+1, cell_number+1, 2))  # initalize solutions
+    # x_array = np.zeros((step_number+1, cell_number+1))      # initalize mesh geometry
 
     #initial conditions
     au = 1
@@ -89,9 +149,7 @@ def run_simulation():
 
     # mesh and linear lagrange elements are created
 
-    msh = mesh.create_unit_interval(comm=MPI.COMM_WORLD,
-                                nx=cell_number,
-                                ghost_mode=GhostMode.shared_facet)
+    msh = create_3d_cell_mesh('cell')
     P1 = element("Lagrange", msh.basix_cell(), 1)
     ME = functionspace(msh, mixed_element([P1, P1]))
     
@@ -128,23 +186,21 @@ def run_simulation():
 
     
     # Output file
-    file1 = XDMFFile(MPI.COMM_WORLD, "Schnakenberg1D/outputu1.xdmf", "w")
+    file1 = XDMFFile(MPI.COMM_WORLD, "Schnakenberg3D/outputu1.xdmf", "w")
     file1.write_mesh(msh)
-    file2 = XDMFFile(MPI.COMM_WORLD, "Schnakenberg1D/outputu2.xdmf", "w")
+    file2 = XDMFFile(MPI.COMM_WORLD, "Schnakenberg3D/outputu2.xdmf", "w")
     file2.write_mesh(msh)
 
     # reporting values
-    l2_norm = []
     u1 = u.sub(0).collapse()
-    
     u2 = u.sub(1).collapse()
     file1.write_function(u1, 0, mesh_xpath=f"/Xdmf/Domain/Grid[@Name='{msh.name}']")
     file2.write_function(u2, 0, mesh_xpath=f"/Xdmf/Domain/Grid[@Name='{msh.name}']")
     u0.x.array[:] = u.x.array
     
-    x_array[0] = msh.geometry.x[:,0]
-    uv_array[0,:,0] = u1.x.array
-    uv_array[0,:,1] = u2.x.array
+    # x_array[0] = msh.geometry.x[:,0]
+    # uv_array[0,:,0] = u1.x.array
+    # uv_array[0,:,1] = u2.x.array
 
 
 
@@ -170,11 +226,6 @@ def run_simulation():
         u1, u2 = ufl.split(u)
         u10, u20 = ufl.split(u0)
         
-        # # Weak formulation (original)
-        # F = ((u1 - u10) / k)*q*dx + d1*inner(grad(u1), grad(q))*dx\
-        # -(gamma*(u1*u1*u2-u1+a))*q*dx \
-        # + ((u2 - u20) / k)*v*dx + d2*inner(grad(u2), grad(v))*dx\
-        # -(gamma*(-u1*u1*u2+b))*v*dx 
         
         # Weak formulation (specific term for mass matrix variation)
         F = u1/k*q*dx + d1*inner(grad(u1), grad(q))*dx\
@@ -203,16 +254,16 @@ def run_simulation():
         print(f"Step {i}: num iterations: {r[0]}")
         
         # reporting values
-        l2_norm.append(np.linalg.norm(u.x.array-u0.x.array)/dt)
         u1 = u.sub(0).collapse()
         u2 = u.sub(1).collapse()
-        x_array[i] = msh.geometry.x[:,0]
-        uv_array[i,:,0] = u1.x.array
-        uv_array[i,:,1] = u2.x.array
+        # x_array[i] = msh.geometry.x[:,0]
+        # uv_array[i,:,0] = u1.x.array
+        # uv_array[i,:,1] = u2.x.array
         
         u0.x.array[:] = u.x.array
         # updating mesh geometry
-        update_msh(msh, v_0, v_1, mid_point)
+        update_msh(msh, v_0, v_1)
+
         old_mass = new_mass
 
         # Save solution to file (VTK)
@@ -226,64 +277,22 @@ def run_simulation():
         file2.write_function(u2, t, mesh_xpath=f"/Xdmf/Domain/Grid[@Name='{msh.name}']")
 
 
-        if l2_norm[-1] < norm_stop:
-            print("l2_norm convergence")
-            break
     
 
 
     file1.close()
     file2.close()
     
-    return time_range, x_array, uv_array
 
 
 
 
-def plot(time_range, x_array, uv_array, spacenum, timenum):
-    """Plotting the solution of the PDE
-
-    Args:
-        time_range (1d array): time of the solutions
-        x_array (2d array): mesh geometry over time
-        uv_array (3d array): values of solutions over time
-        spacenum (int): number of space steps for plot
-        timenum (int): number of time steps for plot
-    """
-    # extracting data at the right place
-    T_steps = np.linspace(0, len(time_range)-1, timenum, dtype=int)
-    x_steps = np.linspace(0, len(x_array[0])-1, spacenum, dtype=int)
-    
-    vmin = np.min(uv_array[:,:,1][T_steps][:,x_steps])
-    vmax = np.max(uv_array[:,:,1][T_steps][:,x_steps])
-    umin = np.min(uv_array[:,:,0][T_steps][:,x_steps])
-    umax = np.max(uv_array[:,:,0][T_steps][:,x_steps])
-    
-    # plotting U
-    ax = plt.figure().add_subplot(projection='3d')
-    plt.title('U')
-    for i in T_steps:
-        ax.scatter(x_array[i][x_steps], uv_array[i,:,0][x_steps], zs=time_range[i], zdir='z', c=uv_array[i,:,0][x_steps], cmap="viridis", edgecolor='none', norm=mplc.Normalize(vmin=umin, vmax=umax))
-    ax.set_xlabel('x')
-    ax.set_ylabel('U(x)')
-    ax.set_zlabel('T')
-    
-    # plotting V
-    ax = plt.figure().add_subplot(projection='3d')
-    plt.title('V')
-    for i in T_steps:
-        ax.scatter(x_array[i][x_steps], uv_array[i,:,1][x_steps], zs=time_range[i], zdir='z', c=uv_array[i,:,1][x_steps], cmap="viridis", edgecolor='none', norm=mplc.Normalize(vmin=vmin, vmax=vmax)) 
-    ax.set_xlabel('x')
-    ax.set_ylabel('V(x)')
-    ax.set_zlabel('T')
-    plt.show()
     
     
     
     
 def main():
-    time_range, x_array, uv_array = run_simulation()
-    plot(time_range, x_array, uv_array, 100, 50)
+    run_simulation()
     
     
     
